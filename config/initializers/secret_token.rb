a722f99c1825a348a018abd318a613c2d008a7fc
George Alonge <georgedelson@gmail.com>
2012-12-22 14:01:34 -0800

    initial commit with readme.md file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluckit::Application.config.secret_token = '2f77703bf603e813c4e86741bd5f9276e1f1a979dc2e13f9826b722775be98c064762d7a09465bd19d641d1056989619dc0d7ba4bb3f268cb8175fd9a45c8823'
